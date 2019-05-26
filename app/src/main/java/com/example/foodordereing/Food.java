6889f79ece13c596d1482817390fb6e6ee458bbf
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-26 16:50:11 +0200

    organizing Food class by adding items to menu and describing viewable attributes to each object in menu

---
+++ app/src/main/java/com/example/foodordereing/Food.java
@@ -0,0 +1,30 @@
+package com.example.foodordereing;
+
+public class Food {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static final Food[] food={
+            new Food("Sushi", "Przykładowe podanie porcji dania.", R.drawable.foodapp_th),
+            new Food("Robaki", "Przykładowe podanie robaków na talerzu", R.drawable.foodapp_ui),
+            new Food("Bezpieczna saładka", "Klasyczny bezpieczny wybór bez wtopy.", R.drawable.foodapp_wj)
+    };
+    private Drink(String name, String description, int imageResourceId){
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+    public String toString(){
+        return this.name;
+    }
+}
