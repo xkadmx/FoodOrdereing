156eb3fcf1b2c614171020fa88ad7bde74e43531
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-28 22:11:47 +0200

    adding ArrayAdapter to activity of FoodCategoryActivity

--- app/src/main/java/com/example/foodordereing/FoodCategoryActivity.java
+++ app/src/main/java/com/example/foodordereing/FoodCategoryActivity.java
@@ -2,6 +2,8 @@ package com.example.foodordereing;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class FoodCategoryActivity extends ListActivity {
@@ -9,6 +11,14 @@ public class FoodCategoryActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ListView listFooods = getListView();
+        ArrayAdapter<Food> listAdapter = new ArrayAdapter<Food>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Food.food);
+        listFooods.setAdapter(listAdapter);
+
+        )
 
     }
 
