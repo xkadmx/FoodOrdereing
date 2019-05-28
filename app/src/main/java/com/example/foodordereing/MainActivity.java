ebeef21a0a684723c6f3b97c95c6cfeeb3b8bff0
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-28 21:41:18 +0200

    adding a ListView to TopLevel Activity (activity_main.xml)
    and
    
    adding OnItemClickListener method to wait then perform an action when click is done
    
    adding an adapter to perform listening to list view

--- app/src/main/java/com/example/foodordereing/MainActivity.java
+++ app/src/main/java/com/example/foodordereing/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.foodordereing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //creating OnItemClickListener object
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                     public void onItemClick(AdapterView<?> listView,
+                                             View itemView,
+                                             int position,
+                                             long id){
+                         if (position==0){
+                             Intent intent = new Intent(MainActivity.this, FoodCategoryActivity.class);
+                             startActivity(intent);
+                         }
+                    }
+
+    };
+    ListView listView = (ListView)findViewById(R.id.list_options);
+    listView.setOnItemClickListener(itemClickListener);
+}}
