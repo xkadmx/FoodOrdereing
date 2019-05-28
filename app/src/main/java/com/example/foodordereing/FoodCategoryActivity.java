35aea0dfdab800da94f352ee9bdca3370c78acaa
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-28 21:55:12 +0200

    adding new Activity class

---
+++ app/src/main/java/com/example/foodordereing/FoodCategoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.foodordereing;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class FoodCategoryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
